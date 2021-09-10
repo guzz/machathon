a33389eda3ddb459407b4f2c9f11601c9aa86ffd
Gustavo Junqueira <gsotero@gmail.com>
2021-09-10 13:46:55 -0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,14 @@
+git clone git@github.com:samuellealb/chat-interface.git
+cd chat-interface
+yarn
+cd ..
+
+git clone git@github.com:guzz/machathon-auth-notify.git
+cd machathon-auth-notify
+yarn
+cd ..
+
+git clone git@github.com:guzz/machathon-main-api.git
+cd machathon-main-api
+yarn
+cd ..
