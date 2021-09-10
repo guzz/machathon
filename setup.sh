5e5aa626a45a0bdb57288bfa4ea846ca67a3a8c1
Gustavo Junqueira <gsotero@gmail.com>
2021-09-10 18:53:01 -0300

    Bot insert and deploy

--- setup.sh
+++ setup.sh
@@ -1,16 +1,29 @@
-git clone git@github.com:samuellealb/chat-interface.git
-cd chat-interface
-yarn
-cd ..
+if [ ! -d "./chat-interface" ]; then
+  git clone git@github.com:samuellealb/chat-interface.git
+  cd chat-interface
+  yarn
+  cd ..
+fi
 
-git clone git@github.com:guzz/machathon-auth-notify.git
-cd machathon-auth-notify
-yarn
-cd ..
+if [ ! -d "./machathon-auth-notify" ]; then
+  git clone git@github.com:guzz/machathon-auth-notify.git
+  cd machathon-auth-notify
+  yarn
+  cd ..
+fi
 
-git clone git@github.com:guzz/machathon-main-api.git
-cd machathon-main-api
-yarn
-cd ..
+if [ ! -d "./machathon-main-api" ]; then
+  git clone git@github.com:guzz/machathon-main-api.git
+  cd machathon-main-api
+  yarn
+  cd ..
+fi
+
+if [ ! -d "./ipe-bot" ]; then
+  git clone git@github.com:renatodelpupo/ipe-bot.git
+  cd ipe-bot
+  yarn
+  cd ..
+fi
 
 yarn
